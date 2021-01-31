8e23ac8eb694ed9dce1b21c89cd755d6f7681849
dhpme <dheeraj.pant@gmail.com>
2021-01-31 17:44:50 +0000

    New changes to Demo2

--- Demo2.java
+++ Demo2.java
@@ -3,5 +3,7 @@ class Demo2
 public static void main(String args[])
 {
 	System.out.println("Java Demo Class 2");
+	System.out.println("Second line in Class 2");
+	System.out.println("3rd line in class 2");
 }
 }
